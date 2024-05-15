5285628855e9ad7be4af47e81e31db4ba76fd1b1
vlad <zippo585@gmail.com>
2024-05-16 02:32:59 +0500

    Fixed docker-compose.yaml

--- db/db.go
+++ db/db.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq" // Импорт драйвера PostgreSQL
 	"h-project/config"
+	"strconv"
 )
 
 type DB struct {
@@ -14,8 +15,12 @@ type DB struct {
 }
 
 func NewDB(ctx context.Context, conf config.Config) (*DB, error) {
-
-	conn, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%d password=%s user=%s dbname=%s sslmode=disable timezone='%s'", conf.Host, conf.Port, conf.Password, conf.User, conf.DBName, conf.TimeZone))
+	port, err := strconv.ParseInt(conf.Port, 10, 0)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return nil, err
+	}
+	conn, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%d password=%s user=%s dbname=%s sslmode=disable timezone='%s'", conf.Host, port, conf.Password, conf.User, conf.DBName, conf.TimeZone))
 	if err != nil {
 		return nil, err
 	}
